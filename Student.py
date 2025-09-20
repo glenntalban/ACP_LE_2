e1797a45576947d83bf32a007bb1e40407162f1e
glenntalban <glenntalban068@gmail.com>
2025-09-20 10:54:04 +0700

    Create Student.py

---
+++ Student.py
@@ -0,0 +1,10 @@
+class student:
+    def __init__(self, student_id, student_name, email, grades=None):
+        self.student_id = str(student_id)
+        self.student_name = str(student_name)
+        self.email = str(email)
+        self.grades=None = str(grades=None)
+        pass
+    
+    def __srt__(self):
+        student 
