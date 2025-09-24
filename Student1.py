0b193b5691279dd5adbaee1d7639ab56e872e6ca
glenntalban <glenntalban068@gmail.com>
2025-09-25 00:12:15 +0700

    Update Student1.py

--- Student1.py
+++ Student1.py
@@ -1,2 +1,10 @@
 class Student:
     def __init__(self, student_id, student_name, email, grades=None, courses=None):
+        self.id_name = (student_id, student_name)
+        self.email = email
+        self.grades = grades or {}
+        self.course = course or set()
+
+    def __str__(self):
+        return
+        
