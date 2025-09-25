3d89af69daa896ab05e590d86ba58d6dacfa9b81
glenntalban <glenntalban068@gmail.com>
2025-09-25 11:46:50 +0700

    Update Student1.py

--- Student1.py
+++ Student1.py
@@ -1,9 +1,9 @@
-class Student:
+class Student: 
     def __init__(self, student_id, student_name, email, grades=None, courses=None):
         self.id_name = (student_id, student_name)
         self.email = email
-        self.grades = grades or {}
-        self.course = course or set()
+        self.grades = grades if grades is not None else {}
+        self.course = courses if courses is not None else set()
 
     def __str__(self):
         return
